a62169e051f2a53c9619ec17ef2664e5e6ec0d8a
r0xylover <64430058+r0xylover@users.noreply.github.com>
2026-07-16 17:20:54 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/inspection/sw.js', { scope: '/inspection/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
